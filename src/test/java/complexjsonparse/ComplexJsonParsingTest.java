5df24d6bb2dd8067a06c7ec64e6160a696ecdd8b
stephania <175607859+stephania-ezra@users.noreply.github.com>
2025-10-23 19:33:42 +0530

    Parsing complex json with validations

--- src/test/java/complexjsonparse/ComplexJsonParsingTest.java
+++ src/test/java/complexjsonparse/ComplexJsonParsingTest.java
@@ -1,6 +1,7 @@
 package complexjsonparse;
 
 import io.restassured.path.json.JsonPath;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
@@ -27,6 +28,44 @@ public class ComplexJsonParsingTest {
             // total amount
             int totalAmount = jsonPath.getInt("dashboard.purchaseAmount");
             System.out.println("Total amount is: " + totalAmount);
+
+            //firstCourseTitle
+
+           String firstCourseTitle = jsonPath.getString("courses[0].title");
+           System.out.println("First Course Title is:"+firstCourseTitle);
+
+           //Print all course title
+
+            for(int i =0 ;i< count ;i++) {
+                String courseTitles = jsonPath.getString("courses[" + i + "].title");
+                int coursePrice = jsonPath.getInt("courses["+i+"].price");
+
+                System.out.println("Course Title is:"+courseTitles);
+                System.out.println("Course Price is:"+coursePrice);
+            }
+
+            //No.of.copies sold by RPA course
+
+            for (int i =0 ;i < count ; i++){
+                String courseTitles = jsonPath.getString("courses[" + i + "].title");
+                if(courseTitles.equalsIgnoreCase("RPA")){
+                    int copies= jsonPath.getInt("courses["+i+"].copies");
+                    System.out.println("No.of copies sold by RPA course is:"+copies);
+                    break;
+                }
+            }
+
+
+            //purchase amount calculation
+            int sum=0;
+           for(int i =0 ;i <count ;i++){
+               int coursePrice = jsonPath.getInt("courses["+i+"].price");
+               int courseCopies = jsonPath.getInt("courses["+i+"].copies");
+               int eachCoursePrice = coursePrice * courseCopies;
+               sum = sum + eachCoursePrice;
+           }
+            System.out.println("sum is:"+sum);
+           Assert.assertEquals(sum,totalAmount);
 
         } catch (IOException e) {
             throw new RuntimeException(e);
